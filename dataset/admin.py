d84e76d8118a7fedba56907152c251433dabae00
Benjamin Mampaey <benjamin.mampaey@oma.be>
2014-08-04 17:05:57 +0200

    Improving dataset app

--- dataset/admin.py
+++ dataset/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from dataset.models import DataSet
 # Register your models here.
+
+class DataSetAdmin(admin.ModelAdmin):
+	list_display = ("name", "instrument", "contact")
+	
+	def get_readonly_fields(self, request, obj=None):
+		if obj:
+			return self.readonly_fields + ("name",)
+		return self.readonly_fields
+
+admin.site.register(DataSet, DataSetAdmin)
+
--- dataset/models.py
+++ dataset/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class DataSet(models.Model):
+	name = models.CharField("Data set name.", max_length=20, primary_key = True)
+	contact = models.CharField(help_text = "Contact email for the data set.", blank=True, null=True, max_length=50)
+	instrument = models.CharField(help_text = "Contact email for the data set.", blank=False, null=False, max_length=70)
+	euv = models.BooleanField(help_text = "Data is EUV.", default = False, blank=True)
+	image = models.BooleanField(help_text = "Data are images.", default = False, blank=True)
+	
+	class Meta:
+		db_table = "data_set"
+		ordering = ["name"]
+		verbose_name = "Data set"
+		verbose_name_plural = "Data sets"
+	
+	def __unicode__(self):
+		return unicode(self.name)
