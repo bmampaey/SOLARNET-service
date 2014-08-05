24cb956db748b0f0b92386ce8049e2b9190b7d40
Benjamin Mampaey <benjamin.mampaey@oma.be>
2014-08-05 14:15:02 +0200

    Added permission check on Dataset admin interface

--- dataset/admin.py
+++ dataset/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 from dataset.models import DataSet
 # Register your models here.
 
@@ -10,6 +10,33 @@ class DataSetAdmin(admin.ModelAdmin):
 		if obj:
 			return self.readonly_fields + ("name",)
 		return self.readonly_fields
+	
+	def has_add_permission(self, request):
+		return request.user.is_superuser
+	
+	def has_change_permission(self, request, obj=None):
+		if request.user.is_superuser:
+			return True
+		elif obj is not None:
+			try:
+				group = Group.objects.get(name=obj.name)
+			except Group.DoesNotExist:
+				return False
+			else:
+				return group in request.user.groups.all()
+		else:
+			return True
+	
+	def has_delete_permission(self, request, obj=None):
+		return request.user.is_superuser
+	
+	def get_queryset(self, request):
+		queryset = super(DataSetAdmin, self).get_queryset(request)
+		if request.user.is_superuser:
+			return queryset
+		else:
+			groups = [group.name for group in request.user.groups.all()]
+			return queryset.filter(name__in=groups)
 
 admin.site.register(DataSet, DataSetAdmin)
 
