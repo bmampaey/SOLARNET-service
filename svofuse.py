993e069bbe05a11ec74ec7e1bed519c11a2824fb
Benjamin Mampaey <benjamin.mampaey@oma.be>
2015-01-14 12:55:04 +0100

    Changing fuse filesystem to use path instead of file handle

--- svofuse.py
+++ svofuse.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+# Common imports
+import sys, os, errno
+import argparse
 import logging
+from datetime import timedelta, datetime
 
-import errno
-import os
-from stat import S_IFDIR, S_IFLNK, S_IFREG
-import sys
-from time import time
-from datetime import datetime
-from fuse import FUSE, FuseOSError, Operations, LoggingMixIn
+from fuse import FUSE, Operations, LoggingMixIn
 import requests, urlparse
-import StringIO
+import django
 
 # This should go
 sys.path.append('/home/benjmam/SOLARNET/SDA')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SDA.settings")
-import django
 django.setup()
 
 from wizard.models import UserDataSelection, DataSelection
-import aia_lev1.views, eit.views, swap.views, hmi_magnetogram
-
-if not hasattr(__builtins__, 'bytes'):
-	bytes = str
+import aia_lev1.views, eit.views, swap.views, hmi_magnetogram.views
 
 class HttpFuse(LoggingMixIn, Operations):
 	
@@ -53,16 +47,6 @@ class HttpFuse(LoggingMixIn, Operations):
 		
 	def dataset_names(self, user_name, selection_name):
 		return DataSelection.objects.order_by().filter(user_data_selection__user__username = user_name, user_data_selection__name=selection_name).values_list('dataset__name', flat=True).distinct() 
-	
-#	def file_names(self, user_name, selection_name, dataset_name):
-#		all_results = set()
-#		for data_selection in self.get_data_selections(user_name, selection_name, dataset_name):
-#			data_locations = data_selection.dataset.data_location_model.objects.filter(meta_data_id__in=data_selection.data_ids)
-#			results = set()
-#			for data_location in data_locations:
-#				results.add(os.path.basename(urlparse.urlparse(data_location.url).path))
-#			all_results.update(results)
-#		return all_results
 	
 	def file_names(self, user_name, selection_name, dataset_name):
 		results = set()
@@ -137,28 +121,26 @@ class HttpFuse(LoggingMixIn, Operations):
 			raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), path)
 	
 	def getattr(self, path, fh=None):
-		#
 		user_name, selection_name, dataset_name, file_name = self.split_path(path)
 		if not self.exists(path):
 			raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), path)
 		elif file_name:
-			return {'st_atime': 0, 'st_ctime': 0, 'st_gid': 0, 'st_mode': 33060, 'st_mtime': 0, 'st_nlink': 1, 'st_size': 1073741824, 'st_uid': 0}
+			return {'st_atime': 0, 'st_ctime': 0, 'st_gid': 131, 'st_mode': 33060, 'st_mtime': 0, 'st_nlink': 1, 'st_size': 1073741824, 'st_uid': 121}
 		elif dataset_name:
 			data_selections = self.get_data_selections(user_name, selection_name, dataset_name)
 			times = [(data_selection.created - datetime(1970, 1, 1)).total_seconds() for data_selection in data_selections]
-			return {'st_atime': max(times), 'st_ctime': min(times), 'st_gid': 0, 'st_mode': 16749, 'st_mtime': max(times), 'st_nlink': 2, 'st_size': len(data_selections), 'st_uid': 0}
+			return {'st_atime': max(times), 'st_ctime': min(times), 'st_gid': 131, 'st_mode': 16749, 'st_mtime': max(times), 'st_nlink': 2, 'st_size': len(data_selections), 'st_uid': 121}
 		elif selection_name:
 			user_data_selection = self.get_user_data_selections(user_name, selection_name)[0]
 			mtime = (user_data_selection.updated - datetime(1970, 1, 1)).total_seconds()
 			ctime = (user_data_selection.created - datetime(1970, 1, 1)).total_seconds()
-			return {'st_atime': mtime, 'st_ctime': ctime, 'st_gid': 0, 'st_mode': 16749, 'st_mtime': mtime, 'st_nlink': 2, 'st_size': len(user_data_selection.data_selections.all()), 'st_uid': 0}
+			return {'st_atime': mtime, 'st_ctime': ctime, 'st_gid': 131, 'st_mode': 16749, 'st_mtime': mtime, 'st_nlink': 2, 'st_size': len(user_data_selection.data_selections.all()), 'st_uid': 121}
 		else:
 			user_data_selections = self.get_user_data_selections(user_name, selection_name)
 			times = [(user_data_selection.created - datetime(1970, 1, 1)).total_seconds() for user_data_selection in user_data_selections]
-			return {'st_atime': max(times), 'st_ctime': min(times), 'st_gid': 0, 'st_mode': 16749, 'st_mtime': max(times), 'st_nlink': 2, 'st_size': len(user_data_selections), 'st_uid': 0}
+			return {'st_atime': max(times), 'st_ctime': min(times), 'st_gid': 131, 'st_mode': 16749, 'st_mtime': max(times), 'st_nlink': 2, 'st_size': len(user_data_selections), 'st_uid': 121}
 	
 	def readdir(self, path, fh):
-		#
 		dirents = ['.', '..']
 		user_name, selection_name, dataset_name, file_name = self.split_path(path)
 		if not self.exists(path):
@@ -235,24 +217,25 @@ class HttpFuse(LoggingMixIn, Operations):
 	# ============
 
 	def open(self, path, flags):
+		#import pdb; pdb.set_trace()
 		if flags & self.FORBIDDEN_OPEN_FLAGS:
 			raise OSError(errno.EACCES, os.strerror(errno.EACCES), path)
 		elif not self.exists(path):
 			raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), path)
-		else:
+		elif path not in self.fd:
 			user_name, selection_name, dataset_name, file_name = self.split_path(path)
 			data_location = self.get_data_locations(user_name, selection_name, dataset_name, file_name).pop()
-			self.last_fd += 1
-			import pdb; pdb.set_trace()
 			request = requests.get(data_location.url)
-			self.fd[self.last_fd] = request.content
-			return self.last_fd
+			self.fd[path] = request.content
+		
+		self.last_fd += 1
+		return self.last_fd
 
 	def create(self, path, mode, fi=None):
 		raise OSError(errno.EACCES, os.strerror(errno.EACCES), path)
 
 	def read(self, path, length, offset, fh):
-		return self.fd[fh][offset:offset+length]
+		return self.fd[path][offset:offset+length]
 
 	def write(self, path, buf, offset, fh):
 		raise OSError(errno.EACCES, os.strerror(errno.EACCES), path)
@@ -261,14 +244,30 @@ class HttpFuse(LoggingMixIn, Operations):
 		raise OSError(errno.EACCES, os.strerror(errno.EACCES), path)
 
 	def release(self, path, fh):
-		if fh in self.fd:
-			del self.fd[fh]
+		if path in self.fd:
+			del self.fd[path]
 
 
-if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print('usage: %s <mountpoint>' % sys.argv[0])
-		sys.exit(1)
 
-	logging.getLogger().setLevel(logging.DEBUG)
-	fuse = FUSE(HttpFuse(), sys.argv[1], foreground=True, encoding="ascii")
+if __name__ == "__main__":
+	
+	# Get the arguments
+	parser = argparse.ArgumentParser(description='Describe me')
+	parser.add_argument('--verbose', '-v', default=False, action='store_true', help='Set the logging level to verbose')
+	parser.add_argument('--debug', '-d', default=False, action='store_true', help='Set the logging level to debug')
+	parser.add_argument('--foreground', '-f', default=False, action='store_true', help='Run the script in foreground (handy for debugging')
+	parser.add_argument('mountpoint', help='The mountpoint for the fuse filesystem.')
+	
+	args = parser.parse_args()
+	
+	# Setup the logging
+	if args.debug:
+		logging.basicConfig(level = logging.DEBUG, format='%(levelname)-8s: %(message)s')
+	elif args.verbose:
+		logging.basicConfig(level = logging.INFO, format='%(levelname)-8s: %(message)s')
+	else:
+		logging.basicConfig(level = logging.CRITICAL, format='%(levelname)-8s: %(message)s')
+	
+	fuse = FUSE(HttpFuse(), args.mountpoint, foreground=args.foreground)
+	
+
