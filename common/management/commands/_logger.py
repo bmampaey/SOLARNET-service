4b9aa25d1e3cbaf28d6832c4cc0a65836b47dcd5
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-02-04 15:20:12 +0100

    Added management command to extract keyword from metadata

--- common/management/commands/_logger.py
+++ common/management/commands/_logger.py
@@ -1,9 +1,15 @@
 class Logger:
 	'''Easy logging for command'''
 	
-	def __init__(self, command):
+	def __init__(self, command, debug = False):
 		self.stdout = command.stdout
 		self.style = command.style
+		# If not debug we replace the debug method with a void method
+		if not debug:
+			self.debug = lambda *args: None
+	
+	def debug(self, msg, *args):
+		self.stdout.write(self.style.NOTICE(msg % args))
 	
 	def info(self, msg, *args):
 		self.stdout.write(self.style.SUCCESS(msg % args))
--- common/management/commands/extract_keywords.py
+++ common/management/commands/extract_keywords.py
@@ -1,15 +1,17 @@
 from datetime import datetime
-import dateutil.parser
+from dateutil.parser import parse as parse_date
 import re
-from collections import defaultdict
+from collections import defaultdict, Counter
+from django.core.management.base import BaseCommand, CommandError
 import pyfits
 
 from _logger import Logger
+from dataset.models import Dataset, Keyword
 
-def column_name(key):
+def column_name(keyword):
 	'''Transform a keyword into an acceptable column name'''
 	# It is more simpler in postgres to have column names in lower case and without spaces
-	return re.sub("\W", "_", key.strip()).lower()
+	return re.sub("\W", "_", keyword.strip()).lower()
 
 def value_type(value):
 	'''Return the string representation of the type'''
@@ -22,7 +24,13 @@ def value_type(value):
 	elif isinstance(value, bool):
 		return "bool"
 	else:
-		return "str"
+		try:
+			parse_date(value)
+		except Exception:
+			return "str"
+		else:
+			return "datetime"
+
 
 unit_pattern =  re.compile(r"\s*(\[\s*(?P<unit>[^\]]*)\s*\])?(?P<comment>.*)\s*")
 def extract_unit(comment):
@@ -36,27 +44,27 @@ def extract_unit(comment):
 		return parts["unit"].strip() if parts["unit"] is not None else "", parts["comment"].strip()
 
 
-def get_keywords(header, excluded = [], log):
+def get_keywords(header, log, excluded = []):
 	'''Extract the keywords information from the header'''
 	
 	keywords = list()
 	comment_index = 0
 	history_index = 0
 	
-	for card in header:
+	for card in header.cards:
 		try:
-			key = card.key
+			keyword = card.keyword
 			value = card.value
 			comment = card.comment
 		except Exception, why:
 			log.error("Could not parse card %s: %s. Skipping.", card, why)
 			continue
 		
-		if key.upper() in excluded:
-			log.info('Skipping excluded keyword %s', key)
+		if keyword.upper() in excluded:
+			log.debug('Skipping excluded keyword %s', keyword)
 			continue
 		
-		elif key.lower() == "history":
+		elif keyword.upper() == "HISTORY":
 			value.strip(" \t\n-")
 			if not value:
 				# We omit empty history
@@ -66,7 +74,7 @@ def get_keywords(header, excluded = [], log):
 			history_index += 1
 		
 		
-		elif key.lower() == "comment":
+		elif keyword.upper() == "COMMENT":
 			value.strip(" \t\n-")
 			if not value:
 				# We omit empty comment
@@ -76,13 +84,13 @@ def get_keywords(header, excluded = [], log):
 			comment_index += 1
 		
 		else:
-			db_column = column_name(key)
+			db_column = column_name(keyword)
 		
 		python_type = value_type(value)
 		unit, description = extract_unit(comment)
 		
 		keywords.append({
-			'key': key,
+			'name': keyword,
 			'db_column': db_column,
 			'python_type': python_type,
 			'unit': unit,
@@ -97,9 +105,10 @@ class Command(BaseCommand):
 	
 	def add_arguments(self, parser):
 		parser.add_argument('dataset', help='The id of the dataset.')
+		parser.add_argument('--max_record', '-m', type = int, default = None, help='Maximum number of record to inspect')
 		parser.add_argument('--comments', '-C', default=False, action='store_true', help='Extract pure comment keywords')
 		parser.add_argument('--history', '-H', default=False, action='store_true', help='Extract history keywords')
-		parser.add_argument('--exclude', '-E', default = ["DATASUM", "CHECKSUM", "SIMPLE", "BITPIX"], nargs='*', help='Keywords to exclude (in small caps)')
+		parser.add_argument('--exclude', '-E', default = ["DATASUM", "CHECKSUM", "SIMPLE", "BITPIX"], nargs='*', help='keywords to exclude (in small caps)')
 		
 	def handle(self, **options):
 		# Parse the options
@@ -108,22 +117,73 @@ class Command(BaseCommand):
 			excluded.append('COMMENT')
 		if not options['history']:
 			excluded.append('HISTORY')
-		import pdb; pdb.set_trace()
-		# Import the dataset models
+		
+		# Create a logger
+		log = Logger(self)
+		
+		# Get the dataset
 		try:
-			models = import_module(options['dataset'] + '.models')
+			dataset = Dataset.objects.get(id = options['dataset'])
+		except Dataset.DoesNotExist:
+			raise CommandError('Dataset %s does not exists in the dataset table' % options['dataset'])
+		
+		# Get the dataset Metadata model
+		try:
+			Metadata = dataset.metadata_model
 		except Exception, why:
-			raise CommandError('Cannot import %s models: %s' % (options['dataset'], why))
+			raise CommandError('Cannot import %s Metadata model: %s' % (options['dataset'], why))
+		
+		# Get the metadata to inspect
+		if options['max_record']:
+			# If there is a max records we take them at random
+			metadatas = Metadata.objects.order_by('?')[:options['max_record']]
+		else:
+			# If the number of records is unlimited, we need to use iterator()) instead of all(), because there can be too many records to fit in memory
+			metadatas = Metadata.objects.iterator()
 		
 		# As keywords can be different from one fits file to another
-		# We collect all the possible info variation 
-		all_keywords = defaultdict(lambda : defaultdict(set))
+		# We count all the possible info variation values
+		all_keywords = defaultdict(lambda : defaultdict(Counter))
 		
-		for metadata in models.Metadata.objects.all():
+		# Extract the keywords and update the list of all keywords
+		for metadata in metadatas:
+			log.info(str(metadata.oid))
 			# Reconstruct header from the metadata fits_header field
 			header = pyfits.Header.fromstring(metadata.fits_header)
-			# Extract the keywords and update the complete list
-			keywords = get_keywords(header, excluded, Logger(self))
+			
+			keywords = get_keywords(header, log, excluded)
 			for keyword in keywords:
 				for info, value in keyword.iteritems():
-					all_keywords[keyword['key']][info].add(value)
+					all_keywords[keyword['name']][info][value] += 1
+		
+		# Ask user to select between possible info values if there is more than one
+		for keyword, infos in all_keywords.iteritems():
+			for info, values in infos.iteritems():
+				if len(values) > 1:
+					selections = values.most_common()
+					total = sum(values.values())
+					while True:
+						print 'Please select best option for keyword',  keyword, info, ':'
+						for i, (value, count) in enumerate(selections):
+							print '{i}. {v} [{c}/{t}]'.format(i=i, v=value, c=count, t = total)
+						selection = raw_input('option number: ')
+						if selection.isdigit() and int(selection) < len(selections):
+							infos[info] = selections[int(selection)][0]
+							break
+						else:
+							print 'Invalid selection', selection
+				else:
+					infos[info] = values.most_common(1)[0][0]
+		
+		import pdb; pdb.set_trace()
+		
+		# Insert the keyword info into the DB
+		for keyword, infos in all_keywords.iteritems():
+			obj, created = Keyword.objects.get_or_create(dataset = dataset, name = keyword, defaults = infos)
+			if created:
+				log.info('Created keyword %s with info %s', keyword, infos)
+			else:
+				log.warning('Keyword %s already exists', keyword)
+				for info, value in infos.iteritems():
+					if getattr(obj, info) != value:
+						log.error('%s differ from existing (%s) to extracted (%s)', info, getattr(obj, info), value)
