d4c280bac1c44cf2b6528bb93a7358bdd4bb6c6d
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-02-15 16:10:36 +0100

    added api urls

--- SDA/urls.py
+++ SDA/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from router import router
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^api/', include(router.urls)),
 	url(r'^dataset/', include('dataset.urls')),
-#	url(r'^wizard/', include('wizard.urls')),
 ]
