90c20626bab9ffc08db5b577820bcca50ffe1852
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-02-29 16:52:57 +0100

    Latest tastypie change in read authorization had to be overriden

--- SDA/resources.py
+++ SDA/resources.py
@@ -3,21 +3,29 @@ from tastypie.authentication import MultiAuthentication, BasicAuthentication, Se
 
 
 class AnonymousGETAuthentication(BasicAuthentication):
-	""" No authentication on GET """
+	''' No authentication on GET '''
 	
 	def is_authenticated(self, request, **kwargs):
-		""" If GET, don't check authentication, otherwise fall back to parent """
-	
+		''' If GET, don't check authentication, otherwise fall back to parent '''
+		
 		if request.method == "GET":
 			return True
 		else:
 			return super(AnonymousGETAuthentication, self).is_authenticated(request, **kwargs)
+
+class AlwaysReadAuthorization(DjangoAuthorization):
+	''' Always allow read '''
+	def read_list(self, object_list, bundle):
+		return object_list
+	
+	def read_detail(self, object_list, bundle):
+		return True
 
 
 class ResourceMeta:
 	"""Base class to set common parameters to all resources"""
 	max_limit = 100
 	authentication = MultiAuthentication(SessionAuthentication(), AnonymousGETAuthentication())
-	authorization = DjangoAuthorization()
+	authorization = AlwaysReadAuthorization()
 	always_return_data = True
 	default_format = 'application/json' # TODO necessary?
