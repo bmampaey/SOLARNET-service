2f247dcdf76dc7a16c459cbb77986db11b64a445
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-04-18 16:50:20 +0200

    improved admin

--- data_selection/admin.py
+++ data_selection/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
 from data_selection.models import UserDataSelection, DataSelection
 
@@ -6,8 +8,22 @@ from data_selection.models import UserDataSelection, DataSelection
 class UserDataSelectionAdmin(admin.ModelAdmin):
 	'''Admin class for the UserDataSelection model'''
 	list_display = ["user", "name", "created"]
-	list_filter = ["user__username"]
-
+	list_filter = ["user__name"]
+	readonly_fields = ['data_selections']
+	
+	def data_selections(self, instance):
+		table_body = format_html_join('\n', '<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>', ((ds.dataset, ds.query_string, ds.number_items, ds.created) for ds in instance.data_selections.all()))
+		
+		if table_body:
+			return (mark_safe('<table>\n')
+				+ mark_safe('<thead><tr><th>Dataset</th><th>Query string</th><th># items</th><th>Created</th></tr></thead>\n')
+				+ mark_safe('<tbody>\n') + table_body + mark_safe('\n</tbody>')
+				+ mark_safe('\n</table>'))
+		else:
+			return mark_safe('<span>None</span>')
+	
+	# short_description functions like a model field's verbose_name
+	data_selections.short_description = "Data selections"
 
 @admin.register(DataSelection)
 class DataSelectionAdmin(admin.ModelAdmin):
