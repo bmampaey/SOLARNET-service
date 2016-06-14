ece02f07a2e7563c3ae33937cc4f149925f90b85
Benjamin Mampaey <benjamin.mampaey@oma.be>
2016-06-14 15:37:16 +0200

    improvement on data and thumbnail accessibility

--- dataset/urls.py
+++ dataset/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from dataset import views
 
 urlpatterns = [
-	url(r'download_data/(?P<dataset_id>[a-z][_a-z0-9]*)/(?P<metadata_oid>\d+)/$', views.DownloadData.as_view(), name='download_data'),
+	url(r'data/(?P<dataset_id>[a-z][_a-z0-9]*)/(?P<metadata_oid>\w+)/$', views.Data.as_view(), name='data'),
+	url(r'thumbnail/(?P<dataset_id>[a-z][_a-z0-9]*)/(?P<metadata_oid>\w+)/$', views.Thumbnail.as_view(), name='thumbnail'),
 ]
--- dataset/views.py
+++ dataset/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 
 from dataset.models import Dataset
 
-class DownloadData(RedirectView):
+class Data(RedirectView):
 	'''View to download the data by looking up it's dataset id and metadata oid'''
 	# TODO allow to download by oid or using filter (see django_filter), if there is more than one gives back the first one
 	http_method_names = [u'get', u'head']
@@ -12,4 +12,15 @@ class DownloadData(RedirectView):
 	def get_redirect_url(self, dataset_id, metadata_oid):
 		dataset = get_object_or_404(Dataset, id=dataset_id)
 		metadata = get_object_or_404(dataset.metadata_model, oid=metadata_oid)
-		return metadata.data_location.url
+		return metadata.data_location.file_url
+
+class Thumbnail(RedirectView):
+	'''View to download the thumbnail by looking up it's dataset id and metadata oid'''
+	# TODO allow to download by oid or using filter (see django_filter), if there is more than one gives back the first one
+	http_method_names = [u'get', u'head']
+	permanent = True
+	
+	def get_redirect_url(self, dataset_id, metadata_oid):
+		dataset = get_object_or_404(Dataset, id=dataset_id)
+		metadata = get_object_or_404(dataset.metadata_model, oid=metadata_oid)
+		return metadata.data_location.thumbnail_url
