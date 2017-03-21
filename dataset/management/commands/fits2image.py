cb839ee69b39690c63e71ca05192e50d991a5c5b
Benjamin Mampaey <benjamin.mampaey@oma.be>
2017-03-21 10:15:28 +0100

    improved commands

---
+++ dataset/management/commands/fits2image.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import sys, os
+import argparse
+import logging
+from datetime import timedelta, datetime
+
+from django.core.management.base import BaseCommand, CommandError
+import numpy, pyfits
+from skimage import io, exposure
+
+class Command(BaseCommand):
+	help = 'Generate a preview image from a fits file, rescaling intensities between a lower and upper percentile'
+	
+	def add_arguments(self, parser):
+		parser.add_argument('--debug', '-d', default=False, action='store_true', help='Set the logging level to debug')
+		parser.add_argument('--outdir', '-o', default='.', help='Directory where to write the results.')
+		parser.add_argument('--extension', '-e', default='jpg', help='Extension type of the result image.')
+		parser.add_argument('--HDU', '-H', default=0, type=int, help='The HDU number of the image')
+		parser.add_argument('--lower_percentile', '-l', default=2, type=int, help='The lower percentile to cut the image')
+		parser.add_argument('--upper_percentile', '-u', default=98, type=int, help='The upper percentile to cut the image')
+		parser.add_argument('filenames', nargs='+', help='The paths of the input files.')
+		
+	def handle(self, **options):
+		
+		log = Logger(self, debug=options['debug'])
+		
+		for filename in options['filenames']:
+			log.info('Converting file %s', filename)
+			fits = pyfits.open(filename)
+			img = fits[options['HDU']].data
+			lower_percentile, upper_percentile = numpy.percentile(img, (options['lower_percentile'], options['upper_percentile']))
+			img_rescale = exposure.rescale_intensity(img, in_range=(lower_percentile, upper_percentile))
+			outfilename, trash = os.path.splitext(os.path.basename(filename))
+			outfilename = os.path.join(options['outdir'], outfilename + '.' + options['extension'])
+			io.imsave(outfilename, img_rescale)
+			log.info('Wrote file %s', outfilename)
+
--- metadata/management/commands/populate_gris.py
+++ metadata/management/commands/populate_gris.py
@@ -8,6 +8,7 @@ from django.core.management.base import BaseCommand, CommandError
 from ..logger import Logger
 
 from metadata.management.records.gris_lev1 import Record
+from dataset.models import DataLocation
 
 base_url = 'http://archive.leibniz-kis.de/pub/gris/'
 
@@ -39,34 +40,45 @@ def get_archive_folder(url, auth=None):
 				archive_folders.append(link + '/level1/')
 	return archive_folders
 
-def get_fits_files(url, auth=None):
-	import pdb; pdb.set_trace()
+def get_file_urls(url, auth=None):
 	archive_folders = get_archive_folder(url, auth)
-	fits_files = list()
+	file_urls = list()
 	for archive_folder in archive_folders:
 		response = requests.get(archive_folder, auth=auth)
 		doc = html.fromstring(response.text)
 		doc.make_links_absolute(archive_folder)
 		
 		for element, attribute, link, pos in doc.iterlinks():
-			if attribute.lower() == 'href' and link.endswith('cc'):
-				fits_files.append(link)
+			if attribute.lower() == 'href' : # and not element.text.lower().startswith('parent directory'):
+				file_urls.append(link)
 	
-	return fits_files
+	return file_urls
 
 
 class Command(BaseCommand):
 	help = 'Populate the Metadata and DataLocation for GRIS'
 	
+	def add_arguments(self, parser):
+		parser.add_argument('--debug', default = False, action='store_true', help='Show debugging info')
+		parser.add_argument('--update', default = False, action='store_true', help='Update metadata even if already present in DB')
+	
 	def handle(self, **options):
 		
-		log = Logger(self)
+		log = Logger(self, debug=options['debug'])
 		
 		# Populate the dataset
-		for file_url in get_fits_files(base_url, auth=Record.auth):
-			log.info('Found fits file at %s', file_url)
-			try:
-				record = Record(file_url, log=log)
-				record.create()
-			except Exception, why:
-				log.error('Error creating record for "%s": %s', file_url, why)
+		for file_url in get_file_urls(base_url, auth=Record.auth):
+			if file_url.endswith('c') or file_url.endswith('r'):
+				log.info('Found fits file at %s', file_url)
+				
+				if not options['update'] and DataLocation.objects.filter(file_url=file_url).exists():
+					log.info('Skipping fits file at %s, already parsed', file_url)
+					continue
+				
+				try:
+					record = Record(file_url, log=log)
+					record.create()
+				except Exception, why:
+					log.error('Error creating record for "%s": %s', file_url, why)
+			else:
+				log.info('Skipping file at %s, not a fits file', file_url)
