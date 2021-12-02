e048d71244e04235bde8cfbf67c16dddb0328949
Benjamin Mampaey <benjamin.mampaey@oma.be>
2021-12-02 11:23:33 +0100

    Added view to generate thumbnail from FITS files

--- dataset/urls.py
+++ dataset/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from api import svo_api
 from dataset.resources import DatasetResource, CharacteristicResource, TelescopeResource, InstrumentResource, KeywordResource, DataLocationResource
-from dataset.views import DataView, ThumbnailView, Image2ThumbnailView
+from dataset.views import DataView, ThumbnailView, Image2ThumbnailView, Fits2ThumbnailView
 
 # Register the dataset resources
 svo_api.register(DatasetResource())
@@ -18,4 +18,5 @@ urlpatterns = [
 	path('data/<str:dataset_name>/<str:metadata_oid>/', DataView.as_view(), name='data'),
 	path('thumbnail/<str:dataset_name>/<str:metadata_oid>/', ThumbnailView.as_view(), name='thumbnail'),
 	path('image2thumbnail/', Image2ThumbnailView.as_view(), name='image2thumbnail'),
+	path('fits2thumbnail/', Fits2ThumbnailView.as_view(), name='fits2thumbnail'),
 ]
--- dataset/views.py
+++ dataset/views.py
@@ -3,12 +3,14 @@ from django.core.files.base import ContentFile
 from django.http import FileResponse, Http404
 from django.shortcuts import get_object_or_404
 from PIL import Image
+from astropy.io import fits
 import requests
+import numpy
 
 
 from dataset.models import Dataset
 
-__all__ = ['DataView', 'ThumbnailView', 'Image2ThumbnailView']
+__all__ = ['DataView', 'ThumbnailView', 'Image2ThumbnailView', 'Fits2ThumbnailView']
 
 # TODO allow to download by oid or using filter (see django_filter), if there is more than one gives back the first one
 # TODO should we check online flag ?
@@ -31,6 +33,7 @@ class ThumbnailView(RedirectView):
 		metadata = get_object_or_404(dataset.metadata_model, oid=metadata_oid)
 		return metadata.data_location.thumbnail_url
 
+
 class Image2ThumbnailView(View):
 	'''View to convert an image to a thumbnail'''
 	
@@ -45,3 +48,33 @@ class Image2ThumbnailView(View):
 		except Exception as why:
 			raise Http404 from why
 		return FileResponse(thumbnail)
+
+
+class Fits2ThumbnailView(View):
+	'''View to convert a FITS file to a thumbnail'''
+	
+	def get(self, request, *args, **kwargs):
+		'''Return a response with the thumbnail'''
+		try:
+			# Read the  image data from the FITS file (astropy.io.fits.open accepts an URL)
+			fits_file = fits.open(request.GET['url'])
+			hdu = fits_file[int(request.GET.get('hdu', 0))]
+			data = hdu.data
+			# Remove the percentiles
+			min_percentile, max_percentile = numpy.percentile(data, [float(request.GET.get('min_percentile', 0)), float(request.GET.get('max_percentile', 100))])
+			data[data < min_percentile] = min_percentile
+			data[data > max_percentile] = max_percentile
+			# Rescale the data to the 0..255 range
+			scaled_data = ((data - min_percentile) * (255. / (max_percentile - min_percentile))).round()
+			# Create a thumbnail from the rescaled data
+			image = Image.fromarray(scaled_data.astype('uint8'))
+			image.thumbnail((512,512))
+			# Write the thumbnail to the in memory file
+			thumbnail = ContentFile(b'', name='thumbnail.jpg')
+			image.save(thumbnail, format='jpeg')
+		except Exception as why:
+			raise Http404 from why
+		else:
+			# The file must be rewinded before sending it
+			thumbnail.seek(0)
+			return FileResponse(thumbnail)
