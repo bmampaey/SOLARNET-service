bc818a6ae12f2d5540fe18820ef563c1c2029e35
Benjamin Mampaey <benjamin.mampaey@oma.be>
2022-06-23 20:04:36 +0200

    Added account app

---
+++ account/views.py
@@ -0,0 +1,67 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
+__all__ = ['ProfileView',' LoginView',' LogoutView',' PasswordChangeView',' PasswordChangeDoneView',' PasswordResetView',' PasswordResetDoneView',' PasswordResetConfirmView', 'PasswordResetCompleteView']
+
+extra_context = {
+	'site_url' : '/',
+	'site_header' : 'SOLARNET Virtual Observatory',
+	'site_title' : 'SVO',
+	'has_permission': True,
+	'is_popup': False,
+	'is_nav_sidebar_enabled': False,
+}
+
+class LoginView(auth_views.LoginView):
+	extra_context = extra_context
+	template_name = 'account/login.html'
+	
+	def get_success_url(self):
+		url = self.get_redirect_url()
+		return url or reverse_lazy('account:profile')
+
+
+class LogoutView(auth_views.LogoutView):
+	extra_context = extra_context
+	next_page = reverse_lazy('account:login')
+
+class PasswordChangeView(auth_views.PasswordChangeView):
+	extra_context = extra_context
+	template_name = 'account/password_change.html'
+	success_url = reverse_lazy('account:password_change_done')
+
+class PasswordChangeDoneView(auth_views.PasswordChangeDoneView):
+	extra_context = extra_context
+	template_name = 'account/password_change_done.html'
+
+class PasswordResetView(auth_views.PasswordResetView):
+	extra_context = extra_context
+	template_name = 'account/password_reset.html'
+	email_template_name = 'account/password_reset_email.html'
+	success_url = reverse_lazy('account:password_reset_done')
+
+class PasswordResetDoneView(auth_views.PasswordResetDoneView):
+	extra_context = extra_context
+	template_name = 'account/password_reset_done.html'
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView):
+	extra_context = extra_context
+	template_name = 'account/password_reset_confirm.html'
+	success_url = reverse_lazy('account:password_reset_complete')
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+	extra_context = extra_context
+	template_name = 'account/password_reset_complete.html'
+	success_url = reverse_lazy('account:password_reset_complete')
+	
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['login_url'] = reverse_lazy('account:login')
+		return context
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+	extra_context = {**extra_context, 'title': 'Profile' }
+	template_name = 'account/profile.html'
+	login_url = reverse_lazy('account:login')
--- project/urls/__init__.py
+++ project/urls/__init__.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 # WARNING for some reason auth urls must come before site urls
 urlpatterns = [
 	path('admin/', include('project.admin.urls')),
+	path('account/', include('account.urls')),
 	path('data_selection/', include('data_selection.urls', namespace='data_selection')),
 	path('', include('dataset.urls', namespace='dataset')),
 	path('', include('api.urls')),
