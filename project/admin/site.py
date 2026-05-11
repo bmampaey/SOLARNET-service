7b2f4a92d6480f9407af436034420d5c8a80e2db
Benjamin Mampaey <benjamin.mampaey@oma.be>
2026-05-11 13:28:47 +0200

    Added users list to group admin

--- project/admin/site.py
+++ project/admin/site.py
@@ -1,8 +1,11 @@
 from functools import partial
+
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin, GroupAdmin
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django.contrib.auth.models import Group, User
+from django.utils.html import format_html, format_html_join
 from tastypie.admin import ApiKeyInline
+
 
 # Customize the AdminSite to set adhoc titles and templates
 class AdminSite(admin.AdminSite):
@@ -13,14 +16,36 @@ class AdminSite(admin.AdminSite):
 	password_change_template = 'admin/password_change.html'
 	password_change_done_template = 'admin/password_change_done.html'
 
+
 site = AdminSite(name='admin')
+
 
 # Add the Tastypie API key inline
 class TastypieUserAdmin(UserAdmin):
 	inlines = UserAdmin.inlines + [ApiKeyInline]
 
+
+class CustomGroupAdmin(GroupAdmin):
+	readonly_fields = ['user_list']
+
+	def user_list(self, obj):
+		users = obj.user_set.all()
+		if not users:
+			return 'No users'
+		return format_html(
+			'<ul>{}</ul>',
+			format_html_join(
+				'\n',
+				'<li>{}</li>',
+				((u.email,) for u in users),
+			),
+		)
+
+	user_list.short_description = 'Users'
+
+
 site.register(User, TastypieUserAdmin)
-site.register(Group, GroupAdmin)
+site.register(Group, CustomGroupAdmin)
 
 # Redefine the register decorator for our SVO admin site
 register = partial(admin.register, site=site)
